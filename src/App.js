ba5e83817b5bbefc907e4f53ff6aa9bfd4180b4d
Ruddy García <ruddygasol94@gmail.com>
2022-08-12 13:31:28 -0500

    Adding ant design lib and initial layout

--- src/App.js
+++ src/App.js
@@ -1,106 +1,86 @@
 import { useState } from 'react';
-import './App.css';
-import Login from './components/Login';
+import { Button, Form, Input, Layout, Modal, Table } from 'antd';
+
+const { Content, Header, Sider } = Layout;
 
 const initialUsers = [
   {
     id: 1,
-    name: 'Ruddy',
-    lastName: 'García',
+    name: "Ruddy",
+    lastName: "García",
     age: 27,
   },
   {
     id: 2,
-    name: 'Alex',
-    lastName: 'González',
+    name: "Alex",
+    lastName: "González",
     age: 20,
   },
   {
     id: 3,
-    name: 'Jhon',
-    lastName: 'Doe',
+    name: "Jhon",
+    lastName: "Doe",
     age: 20,
   },
 ];
 
 function App() {
   const [aUsers, setUsers] = useState(initialUsers);
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
+  const [modalVisible, setModalVisible] = useState(false);
   const [form, setForm] = useState({
     name: '',
     lastName: '',
     age: null,
   });
-  const [screen] = useState('Inicio');
+  const [formRef] = Form.useForm();
 
-  const renderUsers = () =>
-    aUsers.map(oUser => (
-      <tr key={oUser.id}>
-        <td>{oUser.name}</td>
-        <td>{oUser.lastName}</td>
-        <td>{oUser.age}</td>
-        <td>
-          <button onClick={() => handleEdit(oUser.id)}>Editar</button>
-          <button onClick={() => handleDelete(oUser.id)}>Quitar</button>
-        </td>
-      </tr>
-    ));
+  const aColumns = [
+    {
+      dataIndex: 'id',
+      key: 'id',
+      title: 'ID',
+    },
+    {
+      dataIndex: 'name',
+      key: 'name',
+      title: 'Nombre',
+    },
+    {
+      dataIndex: 'lastName',
+      key: 'lastName',
+      title: 'Apellidos',
+    },
+    {
+      dataIndex: 'age',
+      key: 'age',
+      title: 'Edad',
+    },
+    {
+      dataIndex: 'id',
+      key: 'actions',
+      title: 'Acciones',
+      render: sId => (
+        <div>
+          <Button onClick={() => handleEdit(sId)}>Editar</Button>
+          <Button onClick={() => handleDelete(sId)}>Eliminar</Button>
+        </div>
+      ),
+    }
+  ];
 
-  const handleAdd = () => {
-    let aTemp = [...aUsers];
-
-    const maxId1 = aTemp.reduce((anterior, actual) => {
-      return anterior.id > actual.id ? anterior.id : actual.id;
-    });
-
-    // Investigando primero encontre esta forma usando 'reduce' pero quise
-    // hacerlo tambien con 'sort' y con 'map' para experimentar más
-    // segun yo las tres son soluciones validas al problema
-
-    // Usando sort
-    // NOTA: Usar asi el sort cambia el orden de los elementos
-    // const maxId2 = aTemp.sort((a, b) => b.id - a.id)[0].id;
-    // console.log(maxId2);
-
-    // Usando max
-    //const maxId3 = Math.max(...aTemp.map(user => user.id));
-    // console.log(maxId3);
-
-    aTemp.push({
-      ...form,
-      id: maxId1 + 1,
-    });
-
-    setUsers(aTemp);
-    resetForm();
+  const handleDelete = (id) => {
+    setUsers(aUsers.filter((obj) => obj.id !== id));
   };
 
-  const handleDelete = id => {
-    setUsers(aUsers.filter(obj => obj.id !== id));
-  };
-
-  const resetForm = () => {
-    // Creo que no es la forma ideal pero de momento es lo que se me ocurre
-    document.getElementById('formulario').reset();
-    setForm({
-      name: '',
-      lastName: '',
-      age: null,
-    });
-  };
-
-  const handleEdit = id => {
-    // TODO Validar que haya datos en el formulario
-    const index = aUsers.findIndex(obj => obj.id === id);
+  const handleEdit = (id) => {
+    // TODO ajustar edición con formulario en modal
+    const index = aUsers.findIndex((obj) => obj.id === id);
     const aTemp = [...aUsers];
     let edit = { ...aTemp[index] };
 
     // Solo remplazar los valores que tengan algo en el form
     const keys = Object.keys(form);
-    keys.forEach(key => {
+    keys.forEach((key) => {
       if (form[key] !== '' && form[key] !== null) {
         edit[key] = form[key];
       }
@@ -110,72 +90,95 @@ function App() {
     };
 
     setUsers(aTemp);
-    resetForm();
+    formRef.resetFields();
   };
 
-  const handleInputChange = event => {
-    const { target } = event;
-    const { name, value } = target;
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
 
-    setForm({
-      ...form,
-      [name]: value,
+  const onFinish = values => {
+    let aTemp = [...aUsers];
+
+    // validar si el registro tiene id
+    // En caso de que tenga, buscar su posición en aTemp y reemplazar valor
+    // Caso contrario, lógica actual para inserta
+    const maxId1 = aTemp.reduce((anterior, actual) => {
+      return anterior.id > actual.id ? anterior.id : actual.id;
     });
+    aTemp.push({
+      ...values,
+      id: maxId1 + 1,
+    });
+
+    setUsers(aTemp);
+    setModalVisible(false);
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {screen}
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <div>
-            <table border={1}>
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Apellido</th>
-                  <th>Edad</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>{renderUsers()}</tbody>
-            </table>
-          </div>
-          <div>
-            <form id="formulario">
-              <div className="margin-5">
-                <label>Nombre</label>
-                <input name="name" onChange={handleInputChange} type="text" />
-              </div>
-              <div className="margin-5">
-                <label>Apellido</label>
-                <input
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <Modal
+              onCancel={handleCloseModal}
+              onOk={() => formRef?.submit()}
+              title="Nuevo usuario"
+              visible={modalVisible}
+            >
+              <Form
+                name="user-form"
+                initialValues={form}
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                onFinish={onFinish}
+                autoComplete="off"
+                form={formRef}
+              >
+                <Form.Item
+                  label="Nombre"
+                  name="name"
+                  rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                  <Input type="text" />
+                </Form.Item>
+                <Form.Item
+                  label="Apellido"
                   name="lastName"
-                  onChange={handleInputChange}
-                  type="text"
-                />
-              </div>
-              <div className="margin-5">
-                <label>Edad</label>
-                <input name="age" onChange={handleInputChange} type="text" />
-              </div>
-              <button onClick={handleAdd} type="button">
-                Agregar
-              </button>
-            </form>
-          </div>
-        </div>
-        <Login user={user} setUser={setUser} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+                  rules={[{ required: true, message: 'Please input your last name!' }]}
+                >
+                  <Input type="text" placeholder="Escribe tu apellido" />
+                </Form.Item>
+                <Form.Item
+                  label="Edad"
+                  name="age"
+                  rules={[{ required: true, message: 'Please input your last name!' }]}
+                  placeholder="Escribe tu apellido"
+                >
+                  <Input type="number" min={18} max={99} />
+                </Form.Item>
+              </Form>
+            </Modal>
+            <div>
+              <Button onClick={() => setModalVisible(true)}>Nuevo</Button>
+            </div>
+            <Table dataSource={aUsers} columns={aColumns} rowKey={oItem => oItem.id} />
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
